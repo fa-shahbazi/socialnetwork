38bf916dbc42b5a8cba71a5f66072dbf1bb44ad4
root <fashahbazi2020@gmail.com>
2021-09-17 23:27:50 +0430

    Add model

---
+++ friends/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
+
+User = get_user_model()
+
+
+class BaseModel(models.Model):
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    modified_time = models.DateTimeField(_('modified time'), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Relation(BaseModel):
+    from_user = models.ForeignKey(User, related_name='followings', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User,  related_name='followers', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _('relation')
+        verbose_name_plural = _('relations')
+
+    def __str__(self):
+        return "({follower}) >>> ({following})".format(
+            follower =self.from_user.username,
+            following=self.to_user.username,
+        )
