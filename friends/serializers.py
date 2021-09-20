aa15c24439f33aac588a185fc8b6c7e40c62a300
root <fashahbazi2020@gmail.com>
2021-09-20 21:50:25 +0430

    Add serializer for friend

---
+++ friends/serializers.py
@@ -0,0 +1,18 @@
+from friends.models import Relation
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import ValidationError
+from accounts.serializers import UserSerializer
+
+
+class RelationSerializer(serializers.ModelSerializer):
+    from_user = UserSerializer(read_only=True)
+    to_user = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Relation
+        fields = ['from_user', 'to_user', 'created_at', 'updated_at']
+        read_only_fields = ['from_user',]
+
