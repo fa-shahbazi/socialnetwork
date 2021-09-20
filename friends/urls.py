2c9ac763bc3d863c66fc5b3ff32096c0e3ea49bd
root <fashahbazi2020@gmail.com>
2021-09-20 23:52:28 +0430

    Modify urls

--- friends/urls.py
+++ friends/urls.py
@@ -4,7 +4,7 @@ from .views import FollowersListAPIView, UnFollowDestroyAPIView
 app_name = 'friends'
 urlpatterns = [
 
-    path('followers-list/', UnFollowDestroyAPIView.as_view(), name='unfollow'),
+    path('unfollow-list/', UnFollowDestroyAPIView.as_view(), name='unfollow'),
     path('followers-list/', FollowersListAPIView.as_view(), name='followers_list'),
 
 ]
