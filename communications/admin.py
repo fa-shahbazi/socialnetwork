2c4f252abc95d5f1f82e56e9ec04b1c6b4dd45cb
root <fashahbazi2020@gmail.com>
2021-09-20 21:54:09 +0430

    Modify admin for communications

--- communications/admin.py
+++ communications/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from communications.models import Room, Message
 
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('starter',)
+    list_display = ('sender', 'reciever')
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('starter','room')
+    list_display = ('starter', 'friend', 'room')
+
