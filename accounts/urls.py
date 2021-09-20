bfc27a510693b9cbab4c03eaeb68c49823bd2508
root <fashahbazi2020@gmail.com>
2021-09-20 23:39:00 +0430

    Modify accounts url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,7 @@ from accounts import views
 
 app_name = 'accounts'
 urlpatterns = [
-    # path('register-view/', RegisterView.as_view(), name='register_view'),
     path('login-view/', views.UserLoginAPIView.as_view(), name='login_view'),
-    # path('user-api/<str:username>/', UserRetrieveUpdateDestroy.as_view(), name='user-detail'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('confirm-register/', views.RegisterUserConfirm.as_view(), name='confirm_register'),
 ]
