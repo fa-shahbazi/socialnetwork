797c247eccafedeb86bb148f2565797b9e964b5b
root <fashahbazi2020@gmail.com>
2021-09-20 21:51:00 +0430

    Modify models for friends

--- friends/models.py
+++ friends/models.py
@@ -5,24 +5,18 @@ from django.utils.translation import ugettext_lazy as _
 User = get_user_model()
 
 
-class BaseModel(models.Model):
-    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
-    modified_time = models.DateTimeField(_('modified time'), auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class Relation(BaseModel):
+class Relation(models.Model):
     from_user = models.ForeignKey(User, related_name='followings', on_delete=models.CASCADE)
-    to_user = models.ForeignKey(User,  related_name='followers', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_('updated at'), auto_now=True)
 
     class Meta:
+        db_table = 'relation'
+        unique_together = ['from_user', 'to_user']
         verbose_name = _('relation')
         verbose_name_plural = _('relations')
 
     def __str__(self):
-        return "({follower}) >>> ({following})".format(
-            follower =self.from_user.username,
-            following=self.to_user.username,
-        )
+        return f'{self.from_user.username}>{self.to_user.username}'
+
