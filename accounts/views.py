bd28e1fa55a144a3199f3b2403cc93542f21a8a3
root <fashahbazi2020@gmail.com>
2021-09-20 21:54:32 +0430

    Add views for account

--- accounts/views.py
+++ accounts/views.py
@@ -1,57 +1,67 @@
-from rest_framework import generics
-from accounts.serializers import UserSerializer
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-from rest_framework.authentication import TokenAuthentication
-from .permissions import UserOwnerPermission
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.generics import GenericAPIView
 from django.contrib.auth import get_user_model
+from django.core.cache import cache
 
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
 
-class RegisterView(GenericAPIView):
-    serializer_class = UserSerializer
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors)
-
+from accounts.serializers import UserLoginSerializer, UserSerializer, ProfileSerializer
+from .accounts_backend import generate_verification_code
 
 User = get_user_model()
 
 
-class UserListCreateAPIView(generics.ListCreateAPIView):
-    queryset = User.objects.all()
+class RegisterUser(generics.CreateAPIView):
+    permission_classes = AllowAny,
     serializer_class = UserSerializer
-    # permission_classs = UserOwnerPermission
-
-
-    def perfom_create(self, serializer):
-        password = serializer.validated_data['password']
-        serializer.save(password=make_password(password))
-
-
-class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        data = serializer.validated_data
+        username = data.get('username')
+        password = data.get('password')
+        email = data.get('email')
+        cache.set('username', username, timeout=100)
+        cache.set('password', password, timeout=100)
+        cache.set('email', email, timeout=100)
+        code = generate_verification_code()
+        cache.set('code', 'code', timeout=100)
+        print(code)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({'result': 'The activation code was sent!'}, status=status.HTTP_200_OK)
+
+
+class RegisterUserConfirm(generics.CreateAPIView):
+    permission_classes = AllowAny,
     serializer_class = UserSerializer
-    lookup_field = 'username'
-    lookup_url_kwarg = 'username'
-    permission_classs = UserOwnerPermission
-    authentication_classes = TokenAuthentication,
+    queryset = User.objects.all()
 
-    def perform_update(self, serializer):
-        password = serializer.initial_data.get('password')
-        if password is not None:
-            serializer.save(password=make_password(password))
-        else:
-            serializer.save()
+    def create(self, request, *args, **kwargs):
+        username = cache.get('username')
+        password = cache.get('password')
+        email = cache.get('email')
+        if not all([username, password]):
+            return Response({'result': 'The code Was Expired!'})
+        data = {'username': username, 'password': password}
+        if email is not None:
+            data['email'] = email
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+        Token.objects.get_or_create(user=instance)
+        headers = self.get_success_headers(serializer.data)
+        return Response({'result': 'Account Was created!'}, status=status.HTTP_201_CREATED, headers=headers)
 
+
+class UserLoginAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserLoginSerializer
+    permission_classes = [AllowAny, ]
 
 
 
