05d2da41218fb862ff79be67c9a19c387ac20b1e
root <fashahbazi2020@gmail.com>
2021-09-20 21:56:18 +0430

    Delete signal profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.db.models.signals import post_save
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
@@ -13,25 +11,26 @@ class Profile(models.Model):
     )
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 verbose_name=_('user'), on_delete=models.CASCADE)
-    location = models.CharField(max_length=140)
+    location = models.CharField(max_length=140, blank=True)
     gender = models.CharField(max_length=140, choices=CHOICES)
-    avatar = models.ImageField(_('avatar'), blank=True, upload_to='avatars')
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='avatars', null=True)
     bio = models.TextField(_('bio'), blank=True)
-    age = models.PositiveSmallIntegerField(_('age'))
-    phone_number = models.PositiveBigIntegerField(_('phone number'), unique=True)
+    age = models.PositiveSmallIntegerField(_('age'), blank=True, null=True)
+    phone_number = models.PositiveBigIntegerField(_('phone number'), unique=True,
+                                                  blank=True, null=True)
     created_at = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
-
+    updated_at = models.DateTimeField(verbose_name=_('updated at'), auto_now=True)
 
     class Meta:
         db_table = 'profiles'
         verbose_name = _('profiles')
         verbose_name_plural = _('profiles')
 
+    def get_user_id(self):
+        return self.user.id
 
-def save_profile(sender, **kwargs):
-    if kwargs['created']:
-        p1 = Profile(user=kwargs['instance'])
-        p1.save()
+    def get_username(self):
+        return self.user.username
 
-
-post_save.connect(save_profile, sender=User)
+    def __str__(self):
+        return str(self.user)
