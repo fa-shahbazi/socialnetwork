270430837641218e2163c1a05b672822058f5dea
root <fashahbazi2020@gmail.com>
2021-09-20 21:47:28 +0430

    Add urls for posts

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+app_name = 'posts'
+urlpatterns = [
+
+    path('postlist/', views.PostListApiView.as_view(), name='post_list'),
+    path('likecomment/', views.LikeCommentListCreateAPIView.as_view(), name='like_comment'),
+    path('likepost/', views.LikePostListCreateAPIView.as_view(), name='like_post'),
+    path('commentlist/', views.CommentListApiView.as_view(), name='comment_list'),
+
+]
