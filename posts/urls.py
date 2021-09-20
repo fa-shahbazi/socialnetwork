a0a570369cdc9f1c4f227fcd6c025aee4870802b
root <fashahbazi2020@gmail.com>
2021-09-21 00:37:40 +0430

    modify

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,8 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('postlist/', views.PostListApiView.as_view(), name='post_list'),
-    path('likecomment/<int:pk>', views.LikeCommentListCreateAPIView.as_view(), name='like_comment'),
-    path('likepost/<int:pk>', views.LikePostListCreateAPIView.as_view(), name='like_post'),
+    path('<int:pk>/likecomment/', views.LikeCommentListCreateAPIView.as_view(), name='like_comment'),
+    path('<int:pk>/likepost/', views.LikePostListCreateAPIView.as_view(), name='like_post'),
     path('commentlist/', views.CommentListApiView.as_view(), name='comment_list'),
 
 ]
