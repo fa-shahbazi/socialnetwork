2e0e3337a875602064981600698702591c737331
root <fashahbazi2020@gmail.com>
2021-09-21 00:13:38 +0430

    Add  posts urls

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,8 @@ app_name = 'posts'
 urlpatterns = [
 
     path('postlist/', views.PostListApiView.as_view(), name='post_list'),
-    path('likecomment/', views.LikeCommentListCreateAPIView.as_view(), name='like_comment'),
+    path('likecomment/<int:pk>', views.LikeCommentListCreateAPIView.as_view(), name='like_comment'),
     path('likepost/', views.LikePostListCreateAPIView.as_view(), name='like_post'),
-    path('commentlist/', views.CommentListApiView.as_view(), name='comment_list'),
+    path('commentlist/<int:pk>', views.CommentListApiView.as_view(), name='comment_list'),
 
 ]
