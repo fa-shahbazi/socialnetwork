367f05764d5843f027581422010d411966f11ee7
root <fashahbazi2020@gmail.com>
2021-09-20 21:48:12 +0430

    Modify models for posts

--- posts/models.py
+++ posts/models.py
@@ -11,26 +11,17 @@ class Post(models.Model):
     photo = models.ImageField(verbose_name=_('photo'), upload_to='images',
                               blank=True)
     location = models.CharField(verbose_name=_('location'), max_length=30, blank=True)
-    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_('likes'),
-                                   related_name="liker", blank=True, symmetrical=False)
     created_at = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(verbose_name=_("updated time"), auto_now=True)
     is_enabled = models.BooleanField(verbose_name=_("is_enabled"), default=True)
 
+    def __str__(self):
+        return self.user.username
+
     class Meta:
-        ordering = ['-created_at']
         db_table = 'posts'
         verbose_name = _('post')
         verbose_name_plural = _('posts')
-
-    def number_of_likes(self):
-        if self.likes.count():
-            return self.likes.count()
-        else:
-            return 0
-
-    def likes_count(self):
-        return self.pvote.count()
 
     def user_can_like(self, user):
         user_like = user.uvote.all()
@@ -46,22 +37,28 @@ class Comment(models.Model):
                                related_name='user_comments')
     reply = models.ForeignKey('self', verbose_name=_('reply'), on_delete=models.CASCADE,
                               null=True, blank=True, related_name='rcomment')
-    is_reply = models.BooleanField(verbose_name=_('is_reply'), default=False)
     text = models.CharField(verbose_name=_('text'), max_length=100)
     created_at = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
         db_table = 'comments'
         verbose_name = _('comment')
         verbose_name_plural = _('comments')
 
 
-class Vote(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pvote')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE, related_name='uvote')
+class LikePost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_likes')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user} liked {self.post}'
+        return self.user.username
 
+
+class LikeComment(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comment_likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
