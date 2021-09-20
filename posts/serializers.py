9504a359b91ca48bc98c30d2b482edc3e5ab0c72
root <fashahbazi2020@gmail.com>
2021-09-20 21:47:50 +0430

    Add serializer for posts

---
+++ posts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+
+from posts.models import Post, Comment, LikePost, LikeComment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['user', 'body', 'slug', 'photo', 'location', 'likes',
+                  'created_at', 'updated_time', 'is_enabled']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['post', 'author', 'reply', 'is_reply', 'text', 'created_at']
+
+
+class LikePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LikePost
+        fields = ['post', 'user', 'created_at']
+
+
+class LikeCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LikeComment
+        fields = ['comment', 'user', 'created_at']
